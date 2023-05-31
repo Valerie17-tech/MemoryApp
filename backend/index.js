44187c5ade184c1145c532d8074f74bc34eecd41
Valerie17-tech <valeriegeorge1711@gmail.com>
2023-05-31 10:51:30 +0100

    next

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import postRoutes from './routes/posts.js'
 
 const app = express();
 dotenv.config()
@@ -10,6 +11,8 @@ dotenv.config()
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
+app.use('/posts', postRoutes)
+
 
 const PORT =  process.env.PORT || 4001;
 
---
+++ backend/routes/posts.js
@@ -0,0 +1,11 @@
+import express from 'express';
+const router = express.Router();
+import {getPost, createPost, updatePost, deletePost, likePost} from '../controllers/post.js';
+
+router.get('/', getPost)
+router.post('/', createPost)
+router.put('/:id', updatePost)
+router.delete('/:id', deletePost)
+router.put('/:id/likePost', likePost)
+
+export default router;
